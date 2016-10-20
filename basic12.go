844b93d23d209603aab4973467cd67cb10cb0116
Mateus R <mateusrodriguesmoreira20@gmail.com>
2016-10-20 19:42:25 -0200

    basic13

---
+++ basic12.go
@@ -0,0 +1,14 @@
+//variaveis que não tem um valor especifico retornam zero
+
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var j float32
+	var b bool
+	var s string
+	fmt.Print(i,j,b,s)
+}
+
